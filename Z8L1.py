f73fca65dc6de312f6ffbd6464268f99ab2b4e41
Przemysław Dyrcz <282699@uwr.edu.pl>
2019-10-16 15:30:52 +0200

    Zad 8 Lista 1

---
+++ Z8L1.py
@@ -0,0 +1,22 @@
+
+print("podaj a")
+a = input()
+a = int(a)
+print("podaj b")
+b = input()
+b = int(b)
+
+if a < b:
+	print("wartosc b musi byc mniejsza od a")
+	print("podaj b")
+	b = input()
+	b = int(b)
+	
+Dzielenie = int(a/b)
+print("a/b =", Dzielenie)
+reszta_z_dzielenia = a - b*Dzielenie
+print("reszta z dzielenia =", reszta_z_dzielenia)
+
+Z = reszta_z_dzielenia
+wynik = Z*(Z+3)
+print("Z*(Z+3) =",wynik)
