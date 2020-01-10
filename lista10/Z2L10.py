c6bef16a38f85af1f0ceb60cfbe1026353929bb5
Przemysław Dyrcz <282699@uwr.edu.pl>
2020-01-10 10:27:07 +0100

    update structure

--- lista10/Z2L10.py
+++ lista10/Z2L10.py
@@ -1,14 +1,25 @@
 # zad 2 lista 10
 
+from itertools import combinations
+
 l1 = [1, 2, 3] 
 
-class podlisty: 
-    def funk_pl(list1): 
-        sublist = [[]]  
-        for i in range(len(list1) + 1): 
-            for j in range(i + 1, len(list1) + 1):  
-                sub = list1[i:j] 
-                sublist.append(sub) 
-        return sublist 
+#class podlisty: 
+#    def funk_pl(list1): 
+#        sublist = [[]]  
+#        for i in range(len(list1) + 1): 
+#            for j in range(i + 1, len(list1) + 1):  
+#                sub = list1[i:j] 
+#                sublist.append(sub) 
+#        return sublist 
 
-print(podlisty.funk_pl(l1))
+class podlisty: 
+    def sub_lists(my_list):
+        subs = []
+        for i in range(0, len(my_list)+1):
+            temp = [list(x) for x in combinations(my_list, i)]
+            if len(temp)>0:
+                subs.extend(temp)
+        return subs
+
+print(podlisty.sub_lists(l1))
